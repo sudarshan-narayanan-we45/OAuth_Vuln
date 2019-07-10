e2ba8e46ab3b3dbc102a1ce196432948649280e6
sudarshanwe45 <sudarshan.narayanan@we45.com>
2019-07-10 19:15:33 +0530

    Changes to server scripts to allow host binding

--- client/AC_client.py
+++ client/AC_client.py
@@ -91,4 +91,4 @@ if __name__ == '__main__':
   #context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
   #context.load_cert_chain('domain.crt', 'domain.key')
   #app.run(port = 5000, debug = True, ssl_context = context)
-  app.run(port = 5000, debug = True)
+  app.run(port = 5000, host='0.0.0.0',  debug = True)
