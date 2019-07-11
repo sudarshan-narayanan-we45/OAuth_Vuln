8809dcafcf16171c951ec631c963775d3819b644
sudarshanwe45 <sudarshan.narayanan@we45.com>
2019-07-11 18:36:24 +0530

    Commit Galore 2

--- start_app.sh
+++ start_app.sh
@@ -6,6 +6,6 @@ echo -e 'Starting the OAuth Server on port 5001 \n';
 python auth_server/AC_auth_server.py &
 #sleep 3;
 echo -e 'Starting the Client Application on port 5000 \n';
-python client/AC_client.py &
+python client/AC_client.py 
 #sleep 3;
 #exit;
